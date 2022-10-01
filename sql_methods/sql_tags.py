5879778215e329261a5b5065be451aaa3337a6d2
NikiDumb <tentotennot@mail.ru>
2022-10-01 13:18:50 +0300

    no tags

--- sql_methods/sql_tags.py
+++ sql_methods/sql_tags.py
@@ -1,25 +1,48 @@
 import mysql.connector
-from config import host,user,password, db_name,port
+#from config import host,user,password, db_name,port
+
+global slovar
+slovar = {
+    'log'  :  'BIGINT',
+    'name' : 'TEXT',
+    'description' : 'TEXT',
+    'photo' : 'TEXT',
+    'BirthDate' : 'TEXT',
+    'faculty' : 'TEXT',
+    'group_' : 'TEXT',
+    'course' : 'INT'
+}
+
+host = 'localhost'
+user = 'root'
+password = 'root'
+db_name = 'komitet'
+port = 3306
 
 
-def TagsProcess(SomeText, SomeWords):
-	a = 'хэштеги :'
-	for i in range(0, len(SomeWords)):
-		if (SomeText.lower()).find(SomeWords[i])>0:
-			a+=(' #'+SomeWords[i]) 
-	print (f'{InputText}\n{a}')
 
-async def extract_tags(text_):
+def create_sub_list(list_name, columns):
     connection = mysql.connector.connect(
-            host=host,
-            port = port,
-            user=user,
-            passwd=password,
-            database=db_name
+            host='localhost',
+            port = 3306,
+            user='root',
+            passwd='root',
+            database='komitet'
         )
     cursor = connection.cursor()
     try:
-        date_now = datetime.now()
-        cursor.execute("DELETE FROM events WHERE e_date < (%s)", [date_now])
+        table_name = 'sublist' + str(list_name)
+        create_querry = 'CREATE TABLE IF NOT EXISTS ' + table_name + ' ('
+        arr = columns.split('/')
+        for i in range (0, len(arr)):
+            if i == len(arr) - 1:
+                create_querry += arr[i] + ' ' + slovar[arr[i]] + ')'
+                break
+            create_querry += arr[i] + ' ' + slovar[arr[i]] + ','
+        cursor.execute(create_querry)
+        connection.commit()
     finally:
         connection.close()
+
+
+create_sub_list(1, 'photo')
